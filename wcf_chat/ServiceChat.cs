3ddda032433b912e41a5fc648660e0c5d83efe6b
Andrey Ruban <anruban2001@gmail.com>
2023-05-28 18:48:26 +0300

    delet using

--- wcf_chat/ServiceChat.cs
+++ wcf_chat/ServiceChat.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Runtime.Serialization;
 using System.ServiceModel;
-using System.Text;
 
 namespace wcf_chat
 {
